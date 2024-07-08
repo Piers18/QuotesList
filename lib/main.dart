facc1da881a096685989fd08afa1bd497010dcdb
Piers18 <107908190+Piers18@users.noreply.github.com>
2024-07-08 04:17:42 -0500

    push app

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
+
+void main() => runApp(MaterialApp(
+      home: QuoteList(),
+    ));
+
+class QuoteList extends StatefulWidget {
+  const QuoteList({super.key});
+
+  @override
+  State<QuoteList> createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(
+        text: 'Oscar Wilde',
+        author: 'Be yourself; everyone else is already taken'),
+    Quote(
+        text: 'Oscar Wilde',
+        author: 'I have nothing to declare except my genius'),
+    Quote(
+        text: 'Oscar Wilde',
+        author: 'The truth is rarely pure and never simple'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          title: Text('Awesome Quotes'),
+          centerTitle: true,
+          backgroundColor: Colors.redAccent,
+        ),
+        body: Column(
+            children: quotes
+                .map((quote) => QuoteCard(
+                    quote: quote,
+                    delete: () {
+                      setState(() {
+                        quotes.remove(quote);
+                      });
+                    }))
+                .toList()));
+  }
+}
